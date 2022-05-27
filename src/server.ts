65ffaf15d675af3b690a65de005731da68f36160
almerindopaixao <almerindo_paixao@live.com>
2022-05-27 15:58:05 -0300

    style: garantindo estilo de código no Node js com prettier

--- src/server.ts
+++ src/server.ts
@@ -5,26 +5,26 @@ import { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
-    constructor(private port = 3000) {
-        super();
-    }
+  constructor(private port = 3000) {
+    super();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.setupController();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.setupController();
+  }
 
-    private setupController(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public async init(): Promise<void> {
-        this.setupExpress();
-    }
+  public async init(): Promise<void> {
+    this.setupExpress();
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,9 +2,8 @@ import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
 beforeAll(async () => {
-    const server = new SetupServer();
-    await server.init();
-    
-    global.testRequest = supertest(server.getApp());
-});
+  const server = new SetupServer();
+  await server.init();
 
+  global.testRequest = supertest(server.getApp());
+});
